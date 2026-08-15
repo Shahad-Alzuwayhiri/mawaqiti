3387364c67fa5dbd2b1f7b84ad4dc63daaf35c18
Shahd <107875594+Shahad-Alzuwayhiri@users.noreply.github.com>
2026-08-15 03:53:54 +0300

    Fix mobile navbar

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "./index.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
@@ -14,4 +15,4 @@ createRoot(document.getElementById("root")).render(
       <App />
     </BrowserRouter>
   </StrictMode>
-); 
+);
